a8f210059dbe70657d3d827ed7205f46f660e186
fuwenchang <616190282@qq.com>
2022-07-14 06:42:13 +0800

    添加项目文件。

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//builder.Services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+builder.Services.AddOcelot(new ConfigurationBuilder()
+  .AddJsonFile("configuration.json")
+  .Build());
+
+var app = builder.Build();
+app.UseOcelot();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Api_B/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_B.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetApiB")]
+        public string Get(int id)
+        {
+            return "’‚ «Api_B";
+        }
+    }
+}
